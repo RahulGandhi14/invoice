35d0bff8ce68e23ed7567d14a4fb641a542a06b9
Rahul Gandhi <rahulgandhi274@gmail.com>
2023-02-04 15:38:47 +0530

    feat/ view invoice page - invoice body

--- src/components/Invoice/InvoiceBody.tsx
+++ src/components/Invoice/InvoiceBody.tsx
@@ -8,10 +8,23 @@ const Card = styled('div')`
     ${CardStyles}
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
+    row-gap: 1rem;
 `
 
 const Text = styled('p')`
-    ${fontStyle1}
+    ${fontStyle1};
+    margin-bottom: 0.5rem;
+`
+
+const FieldValue = styled('p')`
+    font-weight: bold;
+    font-size: 0.9325rem;
+`
+
+const Dates = styled('div')`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `
 
 const Id = styled(H3)`
@@ -22,12 +35,23 @@ const Id = styled(H3)`
     }
 `
 
-const Address = styled('address')`
+type AddressProps = {
+    textAlign?:
+        | 'start'
+        | 'end'
+        | 'left'
+        | 'right'
+        | 'center'
+        | 'justify'
+        | 'match-parent'
+}
+
+const Address = styled('address')<AddressProps>`
     ${fontStyle2}
     font-style: normal;
     display: flex;
     flex-direction: column;
-    text-align: end;
+    text-align: ${(props) => (props.textAlign ? props.textAlign : 'end')};
     grid-area: 1 / 2 / 2 / 4;
 `
 
@@ -46,6 +70,32 @@ const InvoiceBody = () => {
                 <span>Bradford</span>
                 <span>BD9 4NP</span>
             </Address>
+            <Dates>
+                <div>
+                    <Text>Invoice Date</Text>
+                    <FieldValue>25 Dec 2022</FieldValue>
+                </div>
+                <div>
+                    <Text>Payment Due</Text>
+                    <FieldValue>31 Dec 2022</FieldValue>
+                </div>
+            </Dates>
+            <div>
+                <Text>Bill to</Text>
+                <FieldValue style={{ marginBottom: '0.25rem' }}>
+                    John Cena
+                </FieldValue>
+                <Address textAlign="start">
+                    <span>A, Selborne Mansions</span>
+                    <span>United Kingdom</span>
+                    <span>Bradford</span>
+                    <span>BD9 4NP</span>
+                </Address>
+            </div>
+            <div>
+                <Text>Sent to</Text>
+                <FieldValue>random@gmail.com</FieldValue>
+            </div>
         </Card>
     )
 }
