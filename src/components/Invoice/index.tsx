93421fdfa80dde2588cf29797ed3f3e697e981f2
Rahul Gandhi <rahulgandhi274@gmail.com>
2023-07-23 20:55:25 +0530

    feat/ invoice not found message

--- src/components/Invoice/index.tsx
+++ src/components/Invoice/index.tsx
@@ -10,6 +10,8 @@ import { useParams } from 'react-router-dom'
 import DeletePopup from './DeletePopup'
 import { Variants } from 'framer-motion'
 import InvoiceFooter from './InvoiceFooter'
+import { fontStyle1 } from '../styled/Typography'
+import { H1, H4 } from '../styled/Headings'
 
 const InvoiceWrapper = styled(PageWrapper)`
     height: auto;
@@ -19,6 +21,15 @@ const InvoiceWrapper = styled(PageWrapper)`
         padding: 0;
         margin-bottom: 5.5rem; // Compansate for the footer
     }
+`
+
+const InvoiceNotFoundWrapper = styled('div')`
+    ${fontStyle1}
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    margin: 2rem 0;
 `
 
 const variants: Variants = {
@@ -38,6 +49,8 @@ const variants: Variants = {
 const Invoice = () => {
     const params = useParams<{ id: string }>()
 
+    const [doesInvoiceExist, setDoesInvoiceExist] = useState<boolean>(true)
+
     const invoices = useAppSelector((state) => state.invoice.invoices)
     const editCurrentInvoice = useAppSelector(
         (state) => state.invoice.editCurrentInvoice
@@ -48,7 +61,14 @@ const Invoice = () => {
     >(undefined)
 
     useEffect(() => {
-        setCurrentInvoice(invoices.find((invoice) => invoice.id === params?.id))
+        const idxOfInvoice = invoices.findIndex(
+            (invoice) => invoice.id === params?.id
+        )
+        if (idxOfInvoice === -1) {
+            setDoesInvoiceExist(false)
+        } else {
+            setCurrentInvoice(invoices[idxOfInvoice])
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [editCurrentInvoice, invoices])
 
@@ -62,15 +82,26 @@ const Invoice = () => {
                 exit="exit"
             >
                 <BackBtn />
-                <InvoiceHeader
-                    invoiceStatus={
-                        currentInvoice?.status || EInvoiceStatus.DRAFT
-                    }
-                />
-                <InvoiceBody invoice={currentInvoice} />
-                <InvoiceFooter
-                    status={currentInvoice?.status || EInvoiceStatus.DRAFT}
-                />
+                {doesInvoiceExist ? (
+                    <>
+                        <InvoiceHeader
+                            invoiceStatus={
+                                currentInvoice?.status || EInvoiceStatus.DRAFT
+                            }
+                        />
+                        <InvoiceBody invoice={currentInvoice} />
+                        <InvoiceFooter
+                            status={
+                                currentInvoice?.status || EInvoiceStatus.DRAFT
+                            }
+                        />
+                    </>
+                ) : (
+                    <InvoiceNotFoundWrapper>
+                        <H1>Oops!</H1>
+                        <H4>404 - Invoice not found</H4>
+                    </InvoiceNotFoundWrapper>
+                )}
             </InvoiceWrapper>
         </>
     )
